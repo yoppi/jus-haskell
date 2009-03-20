db31e5daf34f7c6774f470c33e330091cc1d5d5a
yoppi <yoshida@macbook.local>
2009-03-20 17:23:12 +0900

    implemented ver.3 to split each paragraph

--- SlideShow.hs
+++ SlideShow.hs
@@ -3,9 +3,26 @@
 
 module Main where
 
+import Data.List
 import System.Environment
 import qualified System.IO.UTF8 as U
 
+type Line      = String
+type Paragraph = [Line]
+
+paragraphs :: [Line] -> [Paragraph]
+paragraphs = unfoldr phi
+  where phi [] = Nothing
+        phi ls = case break hr ls of
+                   (xs, _:ys) -> Just (xs, ys) 
+                   xsys       -> Just xsys
+        hr l = "-----" `isPrefixOf` l
 
 main :: IO ()
-main = getArgs >>= U.readFile . head >>= U.putStr
+main = inputSetup >>= outputParagraph . head . paragraphs . lines
+
+inputSetup :: IO String
+inputSetup = getArgs >>= U.readFile . head
+
+outputParagraph :: Paragraph -> IO ()
+outputParagraph = U.putStr . unlines
