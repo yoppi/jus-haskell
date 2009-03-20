880b322f83439f0ade2d0c2a3dd9b7e83f851b71
yoppi <yoshida@macbook.local>
2009-03-20 16:36:25 +0900

    add recursive code

---
+++ Recursive.hs
@@ -0,0 +1,16 @@
+stepWays :: Integer -> Integer
+-- n段の一歩手前は(n-1)段または(n-2)段目
+stepWays 0 = 1
+stepWays 1 = stepWays 0
+stepWays n = stepWays (n-1) + stepWays (n-2)
+
+stepWays2 :: Integer -> Integer
+stepWays2 0 = (1, 1)
+stepWays2 n = (b, a+b)
+stepWays2 n = (b, a+b)
+  where
+    (a, b) = stepWasy2 (n-1)
+
+fastStepWays :: Integer -> Integer
+fastStepWays = fst . stepWays2
+
