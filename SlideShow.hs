98fd6bf4efc6173e4278e544e9e45e7c78a012bf
yoppi <yoshida@EM114-48-159-114.pool.e-mobile.ne.jp>
2009-03-20 21:07:52 +0900

    implemented ver.8, to use pause command in text

--- SlideShow.hs
+++ SlideShow.hs
@@ -3,11 +3,14 @@
 
 module Main where
 
+import Codec.Binary.UTF8.String
 import Data.List
 import System.Cmd
 import System.Environment
+import System.Exit
 import System.IO
 import qualified System.IO.UTF8 as U
+import System.Posix
 import System.Timeout
 
 type Line      = String
@@ -31,6 +34,11 @@ initialize = hSetBuffering stdin NoBuffering
            >> hSetBuffering stdout NoBuffering
            >> stdinNoEcho
 
+stdinNoEcho :: IO ()
+stdinNoEcho =   getTerminalAttributes stdInput
+            >>= flip (setTerminalAttributes stdInput) Immediately
+              . flip withoutMode EnableEcho
+ 
 inputSetup :: IO String
 inputSetup = getArgs >>= U.readFile . head
 
@@ -38,22 +46,33 @@ displayParagraph :: Paragraph -> IO ()
 displayParagraph = (pause >>) . (clear >>) . outputParagraph
 
 outputParagraph :: Paragraph -> IO ()
-outputParagraph = mapM_ displayLine
+outputParagraph = mapM_ (dispatchLine displayLine)
+
+dispatchLine :: (Line -> IO ()) -> Line -> IO ()
+dispatchLine dl l
+  | ":p" `isPrefixOf` l = pause
+  | ":c" `isPrefixOf` l = clear
+  | otherwise           = dl l
 
 displayLine :: Line -> IO ()
-displayLine = (>> dilay) . outputLine
+displayLine = (>> delay (10^5)) . (>> newline) . outputLine
 
 outputLine :: Line -> IO ()
 outputLine = mapM_ displayChar
 
 displayChar :: Char -> IO ()
-displayChar = (>> delay (10^4)) . outputChar
+displayChar = (>> delay (10^5)) . outputChar
 
 outputChar :: Char -> IO ()
 outputChar = putStr. encodeString . (:[])
 
 pause :: IO ()
-pause = timeout (-1) getChar >> return ()
+pause = timeout (-1) getChar >>= selector
+
+selector :: Maybe Char -> IO ()
+selector mc = case mc of
+                Just 'q' -> exitSuccess
+                _        -> return ()
 
 clear :: IO ()
 clear = system "clear" >> return ()
